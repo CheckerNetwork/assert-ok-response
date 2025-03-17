38c4b3c6c7f5471b8aa67e895d09afe5be32730e
Miroslav Bajtoš <oss@bajtos.net>
2025-03-17 14:52:27 +0100

    fixup! fix package name
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # assert-ok-response
 
-A tiny helper to assert that response from `fetch()` is ok
+A tiny helper to assert that response from `fetch()` is ok.
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { describe, it } from 'node:test'
 import assert from 'node:assert'
 import { createJsonRpcClient } from './index.js'
 
-describe('tiny-json-rpc', () => {
+describe('assert-ok-response', () => {
   it('should create a JSON-RPC client', () => {
     const client = createJsonRpcClient('http://example.com')
     assert.ok(client)
